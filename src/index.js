345e1804eb1ee43ce60f971cd9c5c7d5c98b28cf
Chross <matias@gmail.com>
2019-06-09 00:30:20 -0400

    I setted the 'skeleton' of the SPA and initialized the Formulario component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './componentes/App.js';
+import App from './componentes/App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
